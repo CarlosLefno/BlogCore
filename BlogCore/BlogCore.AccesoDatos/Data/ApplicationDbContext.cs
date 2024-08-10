4fc21820fb2bf74f1a62aec2fc4d24b5bdca7160
Carlos Eduardo Lefno Vidal <clefnov@gmail.com>
2024-08-09 22:23:43 -0400

    identity1

--- BlogCore/BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
+++ BlogCore/BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace BlogCore.Data
         public DbSet<Articulo> Articulo { get; set; }
 
         public DbSet<Slider> Slider { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
     }
 }
 
---
+++ BlogCore/BlogCoreModels/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage="El nombre es ogligatio")]
+        public string  Nombre { get; set; }
+
+        [Required(ErrorMessage = "La dirección es ogligatia")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "La ciudad es ogligatia")]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El pais es ogligatio")]
+        public string Pais { get; set; }
+
+    }
+}
