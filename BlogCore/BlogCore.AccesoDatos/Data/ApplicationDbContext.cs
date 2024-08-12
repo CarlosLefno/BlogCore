8defeda9fcfde764108262860c5addeb60b26bb1
Carlos Eduardo Lefno Vidal <clefnov@gmail.com>
2024-08-12 00:40:20 -0400

    FuncionaRegiserIdentity

--- BlogCore/BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
+++ BlogCore/BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
@@ -4,12 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlogCore.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
 
         //Poner aqui todos los modelos
         public DbSet<Categoria> Categoria { get; set; }
@@ -19,6 +15,28 @@ namespace BlogCore.Data
         public DbSet<Slider> Slider { get; set; }
 
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
+
+    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    //{
+    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    //        : base(options)
+    //    {
+    //    }
+
+    //    //Poner aqui todos los modelos
+    //    public DbSet<Categoria> Categoria { get; set; }
+
+    //    public DbSet<Articulo> Articulo { get; set; }
+
+    //    public DbSet<Slider> Slider { get; set; }
+
+    //    public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    //}
 }
 
